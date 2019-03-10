a15bbb6cd701789b223dd4446d99a0e45d5a2c23
Paul Nykiel <paul.nykiel@gmail.com>
2019-03-10 16:31:44 +0100

    Started with the Websocket-Server

--- Server/WebSocketServer.hpp
+++ Server/WebSocketServer.hpp
@@ -8,8 +8,26 @@
 #ifndef SOPRANETWORK_WEBSOCKETSERVER_HPP
 #define SOPRANETWORK_WEBSOCKETSERVER_HPP
 
+#include <cstdint>
+#include <thread>
+#include <mutex>
+#include <libwebsockets.h>
+#include "../Util/Listener.hpp"
 
 class WebSocketServer {
+public:
+    explicit WebSocketServer(uint16_t port);
+    void send(std::string text);
+    const util::Listener<std::string> receiveListener;
+
+private:
+    void sendImpl(std::string text);
+    static int callBackHttp(lws_context*, lws*, lws_callback_reasons, void*,void*,std::size_t);
+
+    std::thread workerThread;
+    std::list<std::function<void()>> toCall;
+    std::mutex toCallLock;
+
 
 };
 
--- SopraNetwork.h
+++ SopraNetwork.h
@@ -1,6 +1,6 @@
 #ifndef SOPRA_NETWORK_LIB_H
 #define SOPRA_NETWORK_LIB_H
 
-void hello();
+#include "Server/WebSocketServer.hpp"
 
 #endif
