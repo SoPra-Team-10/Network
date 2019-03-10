42c694674268408172cba1f8ae713ea91f407609
Paul Nykiel <paul.nykiel@gmail.com>
2019-03-10 16:29:02 +0100

    Hopefully fixed CI 0

---
+++ Server/WebSocketServer.cpp
@@ -0,0 +1,8 @@
+/**
+ * @file WebSocketServer.cpp
+ * @author paul
+ * @date 10.03.19
+ * @brief WebSocketServer @TODO
+ */
+
+#include "WebSocketServer.hpp"
---
+++ Server/WebSocketServer.hpp
@@ -0,0 +1,17 @@
+/**
+ * @file WebSocketServer.hpp
+ * @author paul
+ * @date 10.03.19
+ * @brief WebSocketServer @TODO
+ */
+
+#ifndef SOPRANETWORK_WEBSOCKETSERVER_HPP
+#define SOPRANETWORK_WEBSOCKETSERVER_HPP
+
+
+class WebSocketServer {
+
+};
+
+
+#endif //SOPRANETWORK_WEBSOCKETSERVER_HPP
