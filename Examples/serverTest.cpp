c8bba788dd3edc561562d854a2500bcc29770837
Paul Nykiel <paul.nykiel@gmail.com>
2019-03-12 13:58:14 +0100

    Moved ServerTest to examples

--- Examples/serverTest.cpp
+++ Examples/serverTest.cpp
@@ -4,10 +4,13 @@
  * @date 11.03.19
  * @brief Small websockets server
  */
+#include <iostream>
+
 #include <SopraNetwork.h>
 
 int main() {
     network::WebSocketServer server{8080};
+    std::cout << "Started on port 8080" << std::endl;
     while (true) {
 
     }
