48297931dce2caf36f100ad68015c5b59a68fb79
Paul Nykiel <paul.nykiel@gmail.com>
2019-03-13 14:00:38 +0100

    Added the ClientTest, this could actually work!

--- Examples/ClientTest.cpp
+++ Examples/ClientTest.cpp
@@ -4,7 +4,18 @@
  * @date 13.03.19
  * @brief ClientTest @TODO
  */
- 
+#include <iostream>
+
+#include "../Client/WebSocketClient.hpp"
+
+// Or use the installed version:
+//#include <SopraNetwork/WebSocketClient.hpp>
+
 int main() {
+    network::WebSocketClient client{"localhost", "/", 8080, "http-only"};
+
+    while (true) {
+
+    }
     return 0;
 }
